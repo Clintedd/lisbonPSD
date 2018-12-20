ed7f2cbe1a9107d57c8408df033b234ea06f8de9
Clint Lee <l.clint91@gmail.com>
2018-12-19 23:47:01 -0500

    finished html css javascript

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,1 @@
-'use strict';
-
-var app = {};
-
-app.toggle = function () {
-  $('.bar').on('click', function (e) {
-    e.preventDefault();
-    $('.top-nav__nav').css('display', 'block');
-  });
-};
-
-$(document).ready(function () {
-
-  app.toggle();
-});
+"use strict";
